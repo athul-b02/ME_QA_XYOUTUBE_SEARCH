106e89bae70a9d515bae31ff48ee27d61b3a7b57
Athul Baby Thomas <athul.b02@gmail.com>
2024-12-22 16:07:27 +0530

    ME_QA_XYOUTUBE_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -23,7 +24,7 @@ import demo.utils.ExcelDataProvider;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
-public class TestCases extends ExcelDataProvider{ // Lets us read the data
+public class TestCases extends ExcelDataProvider { // Lets us read the data
         ChromeDriver driver;
 
         /*
@@ -56,6 +57,71 @@ public class TestCases extends ExcelDataProvider{ // Lets us read the data
                 driver = new ChromeDriver(options);
 
                 driver.manage().window().maximize();
+        }
+
+        @Test(enabled = true)
+        public void testCase01() {
+                System.out.println("Test Case01: BEGIN");
+                driver.get("https://www.youtube.com/");
+                Assert.assertTrue(driver.getCurrentUrl().contains("youtube.com"), "HomePage url is not valid");
+                Wrappers.clickOnElementWrapper(driver, By.xpath("//a[text()='About']"));
+                Wrappers.displayText(driver, By.xpath("//section[@class='ytabout__content']"));
+                System.out.println("Test Case01: END");
+        }
+
+        @Test(enabled = true)
+        public void testCase02() {
+                System.out.println("Test Case02: BEGIN");
+                driver.get("https://www.youtube.com/");
+                Wrappers.clickOnElementWrapper(driver, By.xpath("//a[@title='Films']"));
+                
+                Wrappers.scrollTillEndRight(driver, By.xpath("//div[@id='right-arrow']//button"));
+
+                SoftAssert sA = new SoftAssert();
+                sA.assertEquals(Wrappers.getMovieRating(driver, 
+                By.xpath("//div[@id='items' and @class='style-scope yt-horizontal-list-renderer']/*[last()]")),
+                 "U","Rating is not U");
+
+                sA.assertTrue(Wrappers.checkMovieCategory(driver,
+                By.xpath("//div[@id='items' and @class='style-scope yt-horizontal-list-renderer']/*[last()]")),"Category does not exist for the movie");
+                sA.assertAll();
+                System.out.println("Test Case02: END");
+        }
+
+        @Test(enabled = true)
+        public void testCase03(){
+                System.out.println("Test Case03: BEGIN");
+                driver.get("https://www.youtube.com/");
+                
+                Wrappers.clickOnElementWrapper(driver, By.xpath("//a[@title='Music']"));
+                Wrappers.scrollTillEndRight(driver, By.xpath("(//div[@id='right-arrow']//button)[1]"));
+                SoftAssert sA = new SoftAssert();
+                
+                sA.assertTrue(Wrappers.checkSongCount(driver, 
+                By.xpath("(//div[@id='items' and @class='style-scope yt-horizontal-list-renderer'])[1]/*[last()]"),
+                 50),
+                 "song count not less than or equal to 50");
+                sA.assertAll();
+                System.out.println("Test Case03: END");
+        }
+        @Test(enabled = true)
+        public void testCase04(){
+                /*
+                 * xpaths list:
+                 * Latest news posts:
+                 * //ytd-rich-section-renderer[@class='style-scope ytd-rich-grid-renderer'][descendant::span[text()='Latest news posts']]
+                 * each news link
+                 * //div[@role='link']
+                 * vote counts
+                 * //span[@id='vote-count-middle']
+                 */
+                
+                System.out.println("Test Case04: BEGIN");
+                driver.get("https://www.youtube.com/");
+                Wrappers.clickOnElementWrapper(driver, By.xpath("//a[@title='News']"));
+                Wrappers.printNewsDetails(driver, 
+                By.xpath("//ytd-rich-section-renderer[@class='style-scope ytd-rich-grid-renderer'][descendant::span[text()='Latest news posts']]"));
+                System.out.println("Test Case04: END");
         }
 
         @AfterTest
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -6,12 +6,136 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+
+    public static void clickOnElementWrapper(ChromeDriver driver, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+    }
+
+    public static void displayText(ChromeDriver driver, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement messageSection = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+
+        String text = messageSection.findElement(By.xpath("./h1")).getText() + "\n"
+        + messageSection.findElement(By.xpath("./p[1]")).getText() + "\n"
+        + messageSection.findElement(By.xpath("./p[2]")).getText();
+
+        System.out.println("About Us Message:-->");
+        System.out.println(text);
+    }
+
+    public static void scrollTillEndRight(ChromeDriver driver, By locator) {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        while (true) {
+            try {
+                WebElement scrollButton = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+
+                scrollButton.click();
+
+                WebElement scrollButton_updated = wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+                if (!scrollButton_updated.isDisplayed())
+                    break;
+                System.out.println("Clicked on the scroll button");
+            } catch (TimeoutException e) {
+                // TODO: handle exception
+                System.out.println(e.getMessage());
+                return;
+            }
+        }
+        System.out.println("Reached the end of the list");
+    }
+
+    public static String getMovieRating(ChromeDriver driver, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement ratingElement = wait.until(ExpectedConditions.presenceOfNestedElementLocatedBy(locator,
+                By.xpath(".//div[contains(@class,'style-type-simple')]/p")));
+        return ratingElement.getText();
+    }
+
+    public static boolean checkMovieCategory(ChromeDriver driver, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        try {
+            wait.until(ExpectedConditions.presenceOfNestedElementLocatedBy(locator,
+                    By.xpath(".//h3/span")));
+            System.out.println("Category detected");
+            return true;
+        } catch (TimeoutException e) {
+            // TODO: handle exception
+            System.out.println("Category not detected");
+            return false;
+        }
+    }
+
+    public static boolean checkSongCount(ChromeDriver driver, By locator,int limit) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            WebElement countElement = wait.until(ExpectedConditions.presenceOfNestedElementLocatedBy(locator,
+                    By.xpath(".//div[@class='badge-shape-wiz__text']")));
+            int currentSongCount = Integer.parseInt(countElement.getText().replaceAll("[^0-9]", ""));
+            System.out.println("Number of songs = "+currentSongCount);
+            if(currentSongCount<=limit)
+                return true;
+            else
+                return false;    
+
+        } catch (TimeoutException e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+            return false;
+
+        }
+    }
+
+    public static void printNewsDetails(ChromeDriver driver, By locator){
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement latestNewsElement = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            
+            List <WebElement> newsLinks = latestNewsElement.findElements(By.xpath(".//div[@role='link']"));
+            newsLinks = newsLinks.subList(0, 3);
+
+            LinkedHashMap<String,Integer> hMap = new LinkedHashMap<>();
+            for(WebElement newsLink : newsLinks){
+                
+                String title = newsLink.findElement(By.xpath(".//*[@id='home-content-text']/span[1]")).getText();
+                int likeCount = 0;
+                if(newsLink.findElements(By.xpath(".//span[@id='vote-count-middle']")).size()!=0){
+                    likeCount = Integer.parseInt(
+                        newsLink.findElement(By.xpath(".//span[@id='vote-count-middle']")).getText().trim()
+                    );
+                }
+                hMap.put(title, likeCount);
+            }
+            
+            int index=0,sum=0;
+            for(Map.Entry<String,Integer> entry : hMap.entrySet()){
+                System.out.println("News Link"+(++index));
+                System.out.println("Content = "+entry.getKey()+"\n");
+                sum+=entry.getValue();
+            }
+            System.out.println("Total number of likes= "+sum);
+
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+        }
+
+    }
+
 }
